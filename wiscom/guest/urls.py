1f50746d381d84b9a133851046eebc3daed20239
hmj6589 <hmj6589@gamil.com>
2023-09-23 22:20:38 +0900

    Feat: guests 앱 생성

---
+++ wiscom/guest/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from .views import GuestListCreateView
+from rest_framework.routers import DefaultRouter
+from guest import views  # guest 앱의 views를 import합니다.
+
+
+router = DefaultRouter()
+router.register(r'guests', views.GuestViewSet)
+
+urlpatterns = [
+    # path('guests/', GuesctListCreateView.as_view(), name='guests-list-create'),
+    path('api/', include(router.urls)),
+
+]
