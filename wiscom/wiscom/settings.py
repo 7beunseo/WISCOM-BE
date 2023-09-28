ac7694bdc7afeb881d1265f717a8ca09098497d2
7beunseo <kimes0403@gmail.com>
2023-09-28 23:01:15 +0900

    Fix: cors 에 해결 세

--- wiscom/wiscom/settings.py
+++ wiscom/wiscom/settings.py
@@ -47,9 +47,12 @@ INSTALLED_APPS = [
     'django_filters',
     'posts',
     'guest',
+
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -57,6 +60,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+	'http://localhost:3000',
 ]
 
 ROOT_URLCONF = 'wiscom.urls'
