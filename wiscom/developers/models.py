964f12f8824135039d2924bd43cb2b2916e425e1
7beunseo <kimes0403@gmail.com>
2023-09-30 01:43:57 +0900

    fix: developers, posts 관계 수정

--- wiscom/developers/models.py
+++ wiscom/developers/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from posts.models import Post
 
 class Developer(models.Model):
-    post_number = models.IntegerField(default=0)
+    post_number = models.ForeignKey(Post, related_name='developer', on_delete=models.CASCADE)
     name = models.CharField(max_length=10)
     image = models.ImageField(upload_to='images/')
     
