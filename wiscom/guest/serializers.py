3a35c8b63047b5623d4633fb3dd1e6044bc81c97
hmj6589 <hmj6589@gamil.com>
2023-11-04 17:57:27 +0900

    Feat

--- wiscom/guest/serializers.py
+++ wiscom/guest/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
+from datetime import datetime  # datetime 모듈 추가
 
 from .models import Guest
 
+
 class CommentSerializer(serializers.ModelSerializer):
+    created_at=serializers.SerializerMethodField()
     class Meta:
         model = Guest
         # fields = ('name', 'comment')
-        fields = '__all__'
+        fields = ['id','content', 'created_at']
+
+    def get_created_at(self, obj):
+        return datetime.now().strftime("%Y-%m-%d")
