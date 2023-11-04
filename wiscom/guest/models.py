4f2c9fb297bbcef6f945dc61b7ef41fe339b33bf
hmj6589 <hmj6589@gamil.com>
2023-11-05 01:53:52 +0900

    Fix : 방명록 created_at 수정

--- wiscom/guest/models.py
+++ wiscom/guest/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Guest(models.Model):
     name = models.CharField(max_length=100)
     content = models.TextField()
-    
+    created_at = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return self.name
